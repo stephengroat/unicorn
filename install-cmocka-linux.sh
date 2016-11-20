8d30d9dd46172330ab34d215ba842eeb359049ab
Stephen <stephengroat@users.noreply.github.com>
2016-11-20 00:25:57 -0800

    Update install-cmocka-linux.sh

--- install-cmocka-linux.sh
+++ install-cmocka-linux.sh
@@ -4,7 +4,7 @@ mkdir -p cmocka
 wget https://cmocka.org/files/1.1/cmocka-1.1.0.tar.xz -O /tmp/cmocka-1.1.0.tar.xz
 tar -xf /tmp/cmocka-1.1.0.tar.xz -C /tmp
 if [[ ! -z $MSYSTEM ]]; then
-cd cmocka && cmake -G "MSYS Makefiles" -DUNIT_TESTING=On /tmp/cmocka-1.1.0 && make && make test
+cd cmocka && cmake -G "MSYS Makefiles" -DUNIT_TESTING=On /tmp/cmocka-1.1.0 && make
 else
 cd cmocka && cmake -DUNIT_TESTING=On /tmp/cmocka-1.1.0 && make && make test
 fi

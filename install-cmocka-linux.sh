bbbb20d8f5216e3952c17d4ee55ff31d1d94084c
Stephen <stephengroat@users.noreply.github.com>
2016-11-19 21:33:39 -0800

    Update install-cmocka-linux.sh

--- install-cmocka-linux.sh
+++ install-cmocka-linux.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 set -ex
-mkdir cmocka
+mkdir -p cmocka
 wget https://cmocka.org/files/1.1/cmocka-1.1.0.tar.xz -O /tmp/cmocka-1.1.0.tar.xz
 tar -xf /tmp/cmocka-1.1.0.tar.xz -C /tmp
-cd cmocka && cmake -DUNIT_TESTING=On /tmp/cmocka-1.1.0 && make && make test
-# cmake builds an so instead of a dll in mingw/msys
 if [[ ! -z $MSYSTEM ]]; then
-cp src/cmocka.so src/cmocka.dll
+cd cmocka && cmake -G "MSYS Makefiles" -DUNIT_TESTING=On /tmp/cmocka-1.1.0 && make && make test
+else
+cd cmocka && cmake -DUNIT_TESTING=On /tmp/cmocka-1.1.0 && make && make test
 fi
 # cmocka does not include headers in build
 cp -R /tmp/cmocka-1.1.0/include/ .
